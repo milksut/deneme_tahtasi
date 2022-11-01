070edaed13a7cc33cb91abb926667e5a3d68c040
milksut <116838611+milksut@users.noreply.github.com>
2022-11-02 00:01:42 +0300

    Add files via upload

---
+++ ders soru 2.cpp
@@ -0,0 +1,66 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main()
+{
+    int sayi1;
+    int sayi2;
+    int sayi3;
+    int cevap;
+    printf("ilk sayiyi giriniz:\n");
+    scanf_s("%d", &sayi1);
+    printf("ikinci sayiyi giriniz:\n");
+    scanf_s("%d", &sayi2);
+    printf("ucuncu sayiyi giriniz:\n");
+    scanf_s("%d", &sayi3);
+    cevap = (sayi1 > sayi2 && sayi1 > sayi3 ? sayi1 : (sayi2 > sayi3 ? sayi2 : sayi3));
+    printf("%d", cevap);
+}*/
+    /*int denemeler(int a)
+    {
+        int i = 1;
+        while (i <= a)
+        {
+            printf("sa\n");
+            i++;
+        }
+        return(0);
+    }
+    int export; int denemeler();int mat_note;
+	int chem_note;
+	int phy_note;
+	printf("matematik notunu girin:\n");
+	scanf_s("%d", &mat_note);
+	printf("kimya notunu girin:\n");
+	scanf_s("%d", &chem_note);
+	printf("fizik notunu girin:\n");
+	scanf_s("%d", &phy_note);
+	if (((mat_note >= 65) && ( chem_note>=50 ) && ( phy_note>=55 )) &&((mat_note + phy_note >= 140) || (mat_note + phy_note + chem_note >=190)))
+		printf("geçti");
+	else
+	{
+		printf("kaldı");
+	}
+	int sayi;
+	int i = 1;
+	bool x = true;
+	scanf_s("%d", &sayi);
+		while (i<=sayi)
+		{
+			i++;
+				if (x)
+				{
+					printf("%d\n" , i);
+					x = false;
+				}
+				else
+				{
+					x = true;
+				}
+		}
+	
+*/
+
+
